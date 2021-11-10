80ebee12eb535a628a91b732cf3d8f9101bede57
ture9200 <ture920016@gmail.com>
2021-11-10 23:37:05 +0900

    commit

--- ex10.js
+++ ex10.js
@@ -1,14 +1,15 @@
 
 const pointArr=[
-    {name:'A',xpos:3,ypos:10},
+    {name:'A',xpos:3,ypos:10},  //A의 x축은 3, y축은 10에 위치
     {name:'B',xpos:7,ypos:6},
     {name:'C',xpos:3,ypos:2},
     {name:'D',xpos:6,ypos:2}
 ]
 
-const target={xpos:6, ypos:3}
+const target={xpos:6, ypos:3}  //기준을 x:6, y:3로 했을때
 
-pointArr.sort((a:{...}, b:{...}) => {
+// 가까운 위치 순으로 배열정렬
+pointArr.sort((a,b) => {
     const distanceA=Math.sqrt(Math.pow(a. xpos-target.xpos, 2)+ Math.pow(a.ypos-target.ypos,2))
     const distanceB=Math.sqrt(Math.pow(b. xpos-target.xpos, 2)+ Math.pow(b.ypos-target.ypos,2))
 
@@ -18,21 +19,14 @@ pointArr.sort((a:{...}, b:{...}) => {
 
     console.log(pointArr)
 
-// for(const pointArrElement of pointArr) {
-//     console.log(pointArrElement)
-//
-//     //target과 pointArrElement 의 거리계산
-//     const distance= Math.sqrt(Math.pow(pointArrElement. xpos-target.xpos, 2)+
-//     Math.pow(pointArrElement.ypos-target.ypos,2))
-//
-//     console.log(distance)
-// }
 
-    // for (let i=0; i<pointArr; i++){
-    //     const pointArrElement=pointArr[i]
-    //     console.log(pointArrElement)
-    // }
+//for(const pointArrElement of pointArr) {  //pointArr의 각 배열들을 출력
+//      console.log(pointArrElement)
+//
+//      //target과 pointArrElement 의 거리계산
+//      const distance= Math.sqrt(Math.pow(pointArrElement. xpos-target.xpos, 2)+
+//      Math.pow(pointArrElement.ypos-target.ypos,2))
+//
+//      console.log(distance)
+//  }
 
-    // for(let i=0; i<pointArr.length; i++){
-    // const point=pointArr[i]
-    // console.log(point)
